5611dde27a57ab6b961e9a650af4267e50e11ad5
Kiversayz <91884457+Kiversayz@users.noreply.github.com>
2024-11-06 08:02:25 +0000

    Залил задание 4.1.1

--- ДЗ/Модуль 4.1 домашнее задание Функции/Модуль 4/Модуль 4/4.1-1.py
+++ ДЗ/Модуль 4.1 домашнее задание Функции/Модуль 4/Модуль 4/4.1-1.py
@@ -5,3 +5,12 @@ if you do so, you are insulting yourself.”
  Bill Gates """
  
 #что нужно сделать то?
+
+text = '''“Don't compare yourself with anyone in this world…
+if you do so, you are insulting yourself.”
+                                        Bill Gates '''
+
+def text_vive (text):
+    print(text)
+
+text_vive(text)
